2a2bc7e7903bc95edc7287d37d93f3b82f055905
Adil Zuhri <106576506+adilzuhri@users.noreply.github.com>
2022-07-19 11:00:43 +0800

    created index resources

---
+++ src/main/java/sg/edu/nus/iss/day12workshop/IndexResource.java
@@ -0,0 +1,20 @@
+package sg.edu.nus.iss.day12workshop;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping (path= {"/indexresource", "/indexresource.html" })
+public class IndexResource {
+    
+    @GetMapping(produces = {"text/html"})
+    public String index (Model model) {
+        model .addAttribute ("currTime", (new Date()). toString());
+        return "indexresource";
+
+    }
+}

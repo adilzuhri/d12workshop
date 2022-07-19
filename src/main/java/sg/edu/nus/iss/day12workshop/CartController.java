f8f821282d99b390ec35d6cf08a594dc7a10660d
Adil Zuhri <106576506+adilzuhri@users.noreply.github.com>
2022-07-19 14:34:52 +0800

    expanded cart features

--- src/main/java/sg/edu/nus/iss/day12workshop/CartController.java
+++ src/main/java/sg/edu/nus/iss/day12workshop/CartController.java
@@ -1,17 +1,19 @@
 package sg.edu.nus.iss.day12workshop;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(path = {"/cart"})
 public class CartController {
     
-    @GetMapping(produces = { "text/html"})
+@GetMapping(produces = "text/html")
     public String displayCart(Model model) {
         CartService cs = new CartService();
         List<Item> cartItems = cs.getShoppingItems();
@@ -19,6 +21,23 @@ public class CartController {
         
         return "cart";
     }
+    @GetMapping("{itemname}")
+    public String filteredCart(
+        @PathVariable(name="itemname", required=true) String itemname,
+        Model model) {
+        CartService cs = new CartService();
+        List<Item> cartItems = cs.getShoppingItems();
 
+        List<Item> filteredItems = new ArrayList<>();
+        for (Item s : cartItems) {
+            if (s.getItemName().contains(itemname)) {
+                filteredItems.add(s);
+            }
+        }
+
+        model.addAttribute("cart", filteredItems);
+
+        return "cart";
+    }
 
 }

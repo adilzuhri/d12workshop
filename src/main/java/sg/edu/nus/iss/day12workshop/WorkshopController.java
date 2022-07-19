cbe74ca02a8b26bc55e6168252db1f39edb09e5c
Adil Zuhri <106576506+adilzuhri@users.noreply.github.com>
2022-07-19 15:53:50 +0800

    added workshop working files

--- src/main/java/sg/edu/nus/iss/day12workshop/WorkshopController.java
+++ src/main/java/sg/edu/nus/iss/day12workshop/WorkshopController.java
@@ -1,7 +1,12 @@
 package sg.edu.nus.iss.day12workshop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WorkshopController {
@@ -11,5 +16,31 @@ public class WorkshopController {
         return "workshop";
     }
 
-    
+    @RequestMapping(path={ "/processNumber"})
+    public String testResults(
+        @RequestParam(name = "inputNumber", defaultValue = "1") Integer unit,
+        Model model) {
+            
+        List<Integer> 1stInt = new ArrayList<>();
+        int loopValue = Integer.parseInt(unit.toString());
+
+        int loop = 1;
+        while (loop <= loopValue) {
+            int result = (int) ((Math.random() * loopValue) + 1);
+        
+            if (1stInt.contains(Integer.valueOf(result))) {
+                1stInt.add(Integer.valueOf(result));
+                loop++;
+            }
+        
+        }
+
+        for (int 1 = 0; i < 1stInt.size(); i++) {
+            System.out.print("==> " + 1stInt.get(1));
+            System.out.println();
+        }
+
+        return "workshop";
+    }
+
 }
